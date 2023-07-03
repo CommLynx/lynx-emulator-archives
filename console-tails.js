284b5048e2fd40f9a6c1578a0178f911d3adfb12
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-07-03 18:10:19 -0400

    Create console-tails.js

---
+++ console-tails.js
@@ -0,0 +1,21 @@
+/*
+Tails is fluffy. Tails is cute. Tails is the best boi.
+You cannot change my mind.
+Therefore, this easter egg is required to exist.
+*/
+
+// These are the Tails photos.
+var Tailses = [
+    {
+        img: './Pinterest/266697609156210092.jpg',
+        title: 'Sonic the Hedgehog',
+        author: 'Archie Comics',
+        description: "Hmmm... what is Tails working on? Find out in the SONIC UNIVERSE 5: THE TAILS ADVENTURE graphic novel out in comic shops today!",
+        artworkurl: 'https://www.pinterest.com/pin/266697609156210092/',
+        authorurl: 'https://www.pinterest.com/archiecomics/',
+        width: 365,
+        height: 546 
+    }
+];
+
+// This easter egg will come back later.
